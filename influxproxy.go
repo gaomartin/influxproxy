5a09c9ed7ee000a1b49b106e8f1e16f15ccf36f9
Daniel Menet <membership@sontags.ch>
2014-07-16 17:07:45 +0200

    implemented expemimental draft of the web interface

--- influxproxy.go
+++ influxproxy.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
+	"github.com/gin-gonic/gin"
 	orch "github.com/influxproxy/influxproxy/orchestrator"
 )
 
@@ -37,9 +37,31 @@ func main() {
 
 	messages, err := o.Start()
 	for _, message := range messages {
-		log.Print(message)
+		log.Println(message)
 	}
 	if err != nil {
 		log.Panic(err)
 	}
+
+	g := gin.Default()
+
+	g.GET("in/:db/:queue/:plugin", func(c *gin.Context) {
+		p := o.Registry.GetPluginByName(c.Params.ByName("plugin"))
+		if p != nil {
+			reply := p.Name //, _ := p.Describe()
+			c.String(200, reply)
+		} else {
+			c.String(404, c.Params.ByName("plugin")+" does not exist")
+		}
+	})
+
+	g.GET("/plugins", func(c *gin.Context) {
+		c.String(200, o.Registry.Print())
+	})
+
+	g.GET("/config", func(c *gin.Context) {
+		c.String(200, o.Config.Print())
+	})
+
+	g.Run(":8080")
 }
