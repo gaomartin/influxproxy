f70ceb26f2947592dccd9914b50244f637f05511
Daniel Menet <membership@sontags.ch>
2014-07-18 22:07:22 +0200

    added default field for Argument

--- plugin/description.go
+++ plugin/description.go
@@ -10,5 +10,6 @@ type Description struct {
 type Argument struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
+	Default     string `json:"default"`
 	Optional    bool   `json:"optional"`
 }
